2d6e3a739e3ab1a75f24b18a423d56af163aba7d
ric <teste@email>
2015-09-11 17:52:10 -0300

    novo arquivo teste3.py

---
+++ teste3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+print "Digite uma string:"
+s = raw_input()
+
+print "  Resultados:"
+print s.upper()
+print s.lower()
+print s.title()
+print s.split()
+
+
